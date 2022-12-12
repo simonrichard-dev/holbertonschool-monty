80012b0a564badc0b5ceb86ff182667fd9e64599
Tnozone <maxnocquet@gmail.com>
2022-12-12 06:22:07 -0800

    push attempt

--- stack_push.c
+++ stack_push.c
@@ -9,5 +9,28 @@
 
 void stack_push(stack_t **stack, unsigned int line_number)
 {
+	(void)line_number;
 
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	node->prev = NULL;
+
+	if (*stack == NULL)
+		node->next = NULL;
+
+	else
+	{
+		node->next = *stack;
+		(*stack)->prev = node;
+	}
+
+	*stack = node;
 }
