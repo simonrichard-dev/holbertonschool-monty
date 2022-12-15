0c9c680847457d5bd055fc50daa489985edd9b4b
Tnozone <maxnocquet@gmail.com>
2022-12-15 02:46:20 -0800

    newline

--- stack_add.c
+++ stack_add.c
@@ -11,7 +11,7 @@ void stack_add(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		error = 1;
 		return;
 	}
